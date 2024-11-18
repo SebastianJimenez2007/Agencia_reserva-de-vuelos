badb67ceaa30bb35faf4da1909827245f1c457eb
SebastianJimenez2007 <sebastianjimenezjjb@gmail.com>
2024-11-18 17:24:33 -0500

    boton de registro

--- src/View/terminosycondiciones.java
+++ src/View/terminosycondiciones.java
@@ -120,7 +120,7 @@ public class terminosycondiciones extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        new Login().setVisible(true);
+        new Registro().setVisible(true);
         this.setVisible(false);
    // TODO add your handling code here:
     }//GEN-LAST:event_jButton4ActionPerformed
